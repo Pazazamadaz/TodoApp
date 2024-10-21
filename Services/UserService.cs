b1d1c9517a3a74d64e3fe2d29256ab7726ce5ea2
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2024-10-21 08:37:54 +0100

    Made username case insensitive in register and authenticate.

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -25,28 +25,23 @@ public class UserService : IUserService
 
     public async Task<string> Authenticate(string username, string password)
     {
-        // Retrieve the user from the database
-        var user = await _context.Users.SingleOrDefaultAsync(x => x.Username == username);
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.Username.ToLower() == username.ToLower());
 
-        // Check if the user exists and verify the password
         if (user == null || !VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
         {
             return null; // Authentication failed
         }
 
-        // Authentication successful, generate JWT token
         return GenerateJwtToken(user);
     }
 
     public async Task<User> Register(UserRegisterDto registrationDto)
     {
-        // Check if the username is already taken
-        if (await _context.Users.AnyAsync(x => x.Username == registrationDto.Username))
+        if (await _context.Users.AnyAsync(x => x.Username.ToLower() == registrationDto.Username.ToLower()))
         {
             return null; // Username already taken
         }
 
-        // Create a new user object
         CreatePasswordHash(registrationDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
 
         var user = new User
