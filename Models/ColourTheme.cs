337a59e611d2856ae497c0d86e11f575c623d9dd
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2024-11-21 16:47:10 +0000

    fixing corrupt migration

--- Models/ColourTheme.cs
+++ Models/ColourTheme.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Colours { get; set; } // JSON of colour property and colour value pairs. E.g. --button-bgcolour paired with #FFF
+        public string Colours { get; set; }
         int? UserId { get; set; }
         bool SysDefined { get; set; } = false;
         bool IsDefault { get; set; } = false;
