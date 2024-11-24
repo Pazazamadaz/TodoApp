7ef24b0809058dea85c8a0ffdef43c1e9cb2586d
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2024-11-24 12:45:38 +0000

    Updated colourtheme to use colour list

---
+++ Models/Colour.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Models
+{
+    public class Colour
+    {
+        public int Id { get; set; }
+        public required string ColourProperty { get; set; }
+        public required string ColourValue { get; set; }
+        public required int ColourThemeId { get; set; }
+    }
+}
--- Models/ColourTheme.cs
+++ Models/ColourTheme.cs
@@ -1,13 +1,15 @@
-﻿namespace TodoApp.Models
+﻿using System.Collections.Generic;
+
+namespace TodoApp.Models
 {
     public class ColourTheme
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public required string Colours { get; set; }
+        public required List<Colour> Colours { get; set; } = new List<Colour>();
         public int? UserId { get; set; }
         public bool SysDefined { get; set; } = false;
-        public bool IsDefault { get; set; } = false;
-        public bool IsActive { get; set; } = false;
+        public bool IsDefault { get; set; }
+        public bool IsActive { get; set; }
     }
 }
