0df8f0a57e68e572ff2d6dce0a68559731774b53
Pazazamadaz <52623509+Pazazamadaz@users.noreply.github.com>
2025-12-31 13:56:15 +0000

    Remoed unceeded normalization of username

--- Dtos/UserBaseDto.cs
+++ Dtos/UserBaseDto.cs
@@ -3,7 +3,7 @@
     public class UserBaseDto
     {
         public string Username { get; set; }
-        public string NormalisedUsername => Username.ToLower();
+        //public string NormalisedUsername => Username.ToLower();
         public bool IsAdmin { get; set; }
     }
 }
